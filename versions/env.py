9594415aaeec83e64d05bf6e9cebf3c90d0b5712
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-02-20 11:08:04 +0200

    finally working allembic env version with proper environment variable access

--- versions/env.py
+++ versions/env.py
@@ -1,9 +1,19 @@
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
+
+from sqlalchemy import create_engine
 from sqlalchemy import pool
 
 from alembic import context
+from models.base import Base
+from models.librarian import Librarian
+from models.reader import Reader
+from models.book import Book
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,13 +28,17 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+def get_url():
+    link = os.environ.get("DATABASE_URL")
+    print(link)
+    return link
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -38,12 +52,12 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url() # Call the get_url function
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+        compare_type=True
     )
 
     with context.begin_transaction():
@@ -57,20 +71,17 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = create_engine(get_url()) # Call the get_url function
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True
         )
 
         with context.begin_transaction():
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
