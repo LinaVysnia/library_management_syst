191be49f99330196e791e6f09280aaf8a82cce26
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-02-20 22:22:04 +0200

    added session factory that uses the same engine all across

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
 
-engine = create_engine(os.environ.get("DATABASE_URL"), echo=True, future=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+engine = create_engine(os.environ.get("DATABASE_URL"), echo=True)
+
+Session = sessionmaker(bind=engine)
