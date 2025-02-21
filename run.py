69ce48c0ee10a10eee4f6052d1f89533c550e455
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-02-21 19:29:07 +0200

    cleaned up run

--- run.py
+++ run.py
@@ -6,19 +6,6 @@ from sqlalchemy.orm import Session
 from data.create_dummy_data import create_dummy_data
 # from UI.login_page_UI import run_login_UI
 
-
-# load_dotenv()  # Load environment variables from .env
-
-# DATABASE_URL = os.environ.get("DATABASE_URL")
-
-# try:
-#     engine = create_engine(DATABASE_URL)
-#     with engine.connect() as connection:
-#         print("Database connection successful!")
-# except Exception as e:
-#     print(f"Database connection failed: {e}")
-
-
 #will create some books if book_data.json or reader_data.json files doen't exist yet
 create_dummy_data()
 
