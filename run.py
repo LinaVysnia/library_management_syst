e04b5e99837b5560ae99945ded3638fadf516d55
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-02-21 19:39:39 +0200

    run.py is even cleaner now

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
-import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-
+from UI.login_page_UI import run_login_UI
 from data.create_dummy_data import create_dummy_data
-# from UI.login_page_UI import run_login_UI
 
 #will create some books if book_data.json or reader_data.json files doen't exist yet
 create_dummy_data()
