f7c4ee7ef21b090793c4a7beecbca12d34acd83c
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-02-21 22:09:03 +0200

    turned off echo for session as debugging printed ui is hell

--- db.py
+++ db.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-engine = create_engine(os.environ.get("DATABASE_URL"), echo=True)
+engine = create_engine(os.environ.get("DATABASE_URL"), echo=False) #change echo True for query printing
 
 Session = sessionmaker(bind=engine)
