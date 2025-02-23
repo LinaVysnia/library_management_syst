5c1e5f507a029b7cc6fd50f4cfbc16589163a6c3
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-02-23 12:32:48 +0200

    added methods for both fetching and printing borrowed, overdue and available books. MUST test if overdue books get fetched correctly

--- app/services/book_service.py
+++ app/services/book_service.py
@@ -1,6 +1,9 @@
 from app.db import Session
 from sqlalchemy import select
 from app.models.book import Book
+from app.models.history import History
+from datetime import datetime
+from app.core.const import days_before_overdue
 
 def get_all_books() -> list[Book]:
     with Session() as session:
@@ -19,29 +22,68 @@ def print_book_inventory():
             print(f"{str(i) + '.':<3} {book}")
         print("\n")
 
-# def get_overdue_book_list(book_obj_list):
+def get_available_books() -> list[Book]:
+    with Session() as session:
+        stmt = select(Book).where(Book.is_borrowed == False)
+        available_books = session.execute(stmt).scalars().all()
+        return available_books
+        available_books = get_available_books(book_obj_list)
 
-#     def is_overdue(book : Book):
+def print_available_books():
 
-#         if not book.is_borrowed:
-#             return False 
-#         #book.history[-1][0] gets the latest history entry with [-1] and the time from history item touple with [0]
-#         elif (datetime.now() - book.history[-1][0]).days > days_before_overdue: 
-#             return True
-#         else:
-#             return False
+    available_books = get_available_books()
 
-#     overdue_book_list = list(filter(lambda book: is_overdue(book), book_obj_list))
-#     return overdue_book_list
+    if available_books:
+        print(f"There are {len(available_books)} books present in the library:")
+        for i, book in enumerate(available_books, 1):
+            print(f"{i}. {book}")
+        print() 
 
-# def print_overdue_books(book_obj_list):
-#     overdue_book_list = get_overdue_book_list(book_obj_list)
+    else:
+        print("All the books in the library are borrowed\n")
+  
+def get_borrowed_books() -> list[Book]:
+    with Session() as session:
+        stmt = select(Book).where(Book.is_borrowed == True)
+        borrowed_books = session.execute(stmt).scalars().all()
+        return borrowed_books
 
-#     if len(overdue_book_list) == 0:
-#         print("There are no overdue books in the library\n")
-#     else:
-#         print(f"There are {len(overdue_book_list)} book(s) overdue:")
-#         for i, book in enumerate(overdue_book_list, 1):
-#             print(f"{i}. {book}.")
-#             print(f"Late reader's card ID is : {book.history[-1][1]}")
-#             print(f"Overdue by {((datetime.now() - book.history[-1][0]).days) - days_before_overdue} days\n")
+def print_borrowed_books():
+
+    borrowed_books = get_borrowed_books()
+
+    if borrowed_books:
+        print(f"There are {len(borrowed_books)} books borrowed from the library:")
+        for i, book in enumerate(borrowed_books, 1):
+            print(f"{i}. {book}")
+            # return_date = (book.history[-1][0] + timedelta(days = days_before_overdue)).strftime("%Y %m %d, %H:%M")
+            # print(f"it has to be returned by {return_date}\n")
+        print()
+
+    else:
+        print("No the books are borrowed\n")
+
+def get_overdue_books() -> list[(Book, datetime)]:
+
+    with Session() as session:
+        stmt = (select(Book, History.due_date)
+                .join(History, Book.id == History.book_id)
+                .where(History.returned_on == None)
+                .where(History.due_date < datetime.today()))
+        
+        overdue_books = session.execute(stmt).all()
+
+    return overdue_books
+
+def print_overdue_books():
+
+    overdue_book_list = get_overdue_books()
+
+    if overdue_book_list:
+        print(f"There are {len(overdue_book_list)} book(s) overdue:")
+        for i, book, due_date in enumerate(overdue_book_list, 1):
+            print(f"{i}. {book}.")
+            print(f"Overdue by {((datetime.now() - due_date).days) - days_before_overdue} days\n")
+        return
+
+    print("There are no overdue books in the library\n")
