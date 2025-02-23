33ad3860d29dc29d6d351f8db6e6e92cfe783913
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-02-23 21:55:48 +0200

    added logic to not select deleted books, added better formatted printing, better method output hinting. Updated every single method to date..

--- app/services/book_service.py
+++ app/services/book_service.py
@@ -7,7 +7,7 @@ from app.core.const import days_before_overdue
 
 def get_all_books() -> list[Book]:
     with Session() as session:
-        stmt = select(Book)
+        stmt = select(Book).where(Book.is_deleted == False)
         all_books = session.execute(stmt).scalars().all()
         return all_books
 
@@ -18,13 +18,14 @@ def print_book_inventory():
         print("There are no books owned by the library")
     else:
         print(f"There are {len(book_obj_list)} books in the inventory:\n")
+        print(f"{'Title':^30}{'Author':^20}{'Publishing year:'}")
         for i, book in enumerate(book_obj_list, 1):
             print(f"{str(i) + '.':<3} {book}")
         print("\n")
 
 def get_available_books() -> list[Book]:
     with Session() as session:
-        stmt = select(Book).where(Book.is_borrowed == False)
+        stmt = select(Book).where(Book.is_borrowed == False).where(Book.is_deleted == False)
         available_books = session.execute(stmt).scalars().all()
         return available_books
         available_books = get_available_books(book_obj_list)
@@ -35,6 +36,7 @@ def print_available_books():
 
     if available_books:
         print(f"There are {len(available_books)} books present in the library:")
+        print(f"{'Title':^30}{'Author':^20}{'Publishing year:'}")
         for i, book in enumerate(available_books, 1):
             print(f"{i}. {book}")
         print() 
@@ -42,35 +44,40 @@ def print_available_books():
     else:
         print("All the books in the library are borrowed\n")
   
-def get_borrowed_books() -> list[Book]:
+def get_borrowed_books() -> list[tuple[Book, History]]:
     with Session() as session:
-        stmt = select(Book).where(Book.is_borrowed == True)
-        borrowed_books = session.execute(stmt).scalars().all()
+        stmt = select(Book, History).join(History, Book.id == History.book_id).where(Book.is_borrowed == True).where(Book.is_deleted == False)
+        borrowed_books = session.execute(stmt).all()
         return borrowed_books
 
 def print_borrowed_books():
 
     borrowed_books = get_borrowed_books()
+    print(borrowed_books)
 
     if borrowed_books:
         print(f"There are {len(borrowed_books)} books borrowed from the library:")
-        for i, book in enumerate(borrowed_books, 1):
+        print(f"{'Title':<30}{'Author':<20}{'Publishing year:'}")
+        for i, (book, history) in enumerate(borrowed_books, 1):
             print(f"{i}. {book}")
-            # return_date = (book.history[-1][0] + timedelta(days = days_before_overdue)).strftime("%Y %m %d, %H:%M")
-            # print(f"it has to be returned by {return_date}\n")
+            print(f"Borrowed by reader no: {history.reader_id} it has to be returned by {history.due_date}\n")
+            if history.due_date < datetime.today():
+                print("The book is overdue! The reader won't be able to borrow any more books while the book isn't returned.")
         print()
 
     else:
         print("No the books are borrowed\n")
 
-def get_overdue_books() -> list[(Book, datetime)]:
+def get_overdue_books(reader_id = None) -> list[tuple[Book, datetime]]:
 
     with Session() as session:
         stmt = (select(Book, History.due_date)
                 .join(History, Book.id == History.book_id)
                 .where(History.returned_on == None)
-                .where(History.due_date < datetime.today()))
-        
+                .where(History.due_date < datetime.today())
+                .where(Book.is_deleted == False))
+        if reader_id:
+            stmt = stmt.where(History.reader_id == reader_id)
         overdue_books = session.execute(stmt).all()
 
     return overdue_books
@@ -81,9 +88,27 @@ def print_overdue_books():
 
     if overdue_book_list:
         print(f"There are {len(overdue_book_list)} book(s) overdue:")
+        print(f"{'Title':^30}{'Author':^20}{'Publishing year:'}")
         for i, book, due_date in enumerate(overdue_book_list, 1):
             print(f"{i}. {book}.")
             print(f"Overdue by {((datetime.now() - due_date).days) - days_before_overdue} days\n")
         return
 
     print("There are no overdue books in the library\n")
+
+def borrow_book(book_id, reader_id, due_date = None):
+    with Session() as session:
+        book_query = select(Book).where(Book.id == book_id).where(Book.is_borrowed == False)
+        book = session.scalars(book_query).one_or_none()
+
+        if not book:
+            raise Exception('book is already borrowed or does not exist')
+        
+        book.is_borrowed = True
+
+        history = History(reader_id = reader_id, book_id = book_id)
+        if due_date:
+            history.due_date = due_date
+        session.add(history)
+        
+        session.commit()
